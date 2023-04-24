1e7b8b989cf81bbc84fa7c44a74f831e11426fbe
Nicolás Cossío Miravalles <nicocossiom@gmail.com>
2023-04-24 11:13:45 +0200

    app codebase (electron+vite+react)

---
+++ Code/IPFShare/src/App.tsx
@@ -0,0 +1,10 @@
+console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
+
+function App() {
+  return (
+    <div className='App'>
+    </div>
+  )
+}
+
+export default App
